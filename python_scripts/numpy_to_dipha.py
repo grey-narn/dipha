0fbaff72074acec6993f63faae7d863ff2c9327c
Arnur Nigmetov <anigmetov@lbl.gov>
2021-02-22 17:57:49 -0800

    Add python script to convert numpy input to dipha

--- python_scripts/numpy_to_dipha.py
+++ python_scripts/numpy_to_dipha.py
@@ -2,17 +2,17 @@
 
 import numpy as np
 import os.path
-import math
-import argh
+import sys
 
 
-def save_numpy_array_in_dipha_format(array, dipha_fname, type_to_save=np.float64, negate=False):
+def convert_np_to_dipha(np_fname, dipha_fname, type_to_save=np.float64, negate=False):
     if os.path.exists(dipha_fname):
         print("File exists, skipping ", dipha_fname)
         return
     else:
-        print(h5_fname, " -> ", dipha_fname)
+        print(np_fname, " -> ", dipha_fname)
 
+    array = np.load(np_fname)
 
     DIPHA_MAGIC=np.array([8067171840], dtype=np.int64)
     DIPHA_IMAGE_DATA=np.array([1], dtype=np.int64)
@@ -36,36 +36,10 @@ def save_numpy_array_in_dipha_format(array, dipha_fname, type_to_save=np.float64
         f.write(array_f.tobytes(order='F'))
 
 
-
-def dipha_fname_from_h5_fname(h5_fname, fields, negate, type_to_save):
-    dipha_fname = h5_fname[:-5] + "_" + fields
-
-    if type_to_save == np.float32:
-        dipha_fname += ".f32"
-    else:
-        assert type_to_save == np.float64
-        dipha_fname += ".f64"
-
-    if negate:
-         dipha_fname += ".us.dipha"
-    else:
-        dipha_fname += ".ls.dipha"
-
-    dipha_fname = os.path.basename(dipha_fname)
-    dipha_fname = os.path.join("$SCRATCH/striped_dataset/ldrd_cosmology/", dipha_fname)
-    dipha_fname = os.path.expandvars(dipha_fname)
-    return dipha_fname
-
-
 if __name__ == "__main__":
-    for fields in ["baryon_density", "baryon_density_dark_matter_density"]:
-        for type_to_save in [np.float32, np.float64]:
-            with open('hdf5_files_list_all.txt', 'r') as ff:
-                for line in ff:
-                    h5_fname = line.rstrip()
-                    cosmology = omegas_from_fname(h5_fname)
-                    data = read_fields(h5_fname, fields, cosmology)
-                    data = data.astype(type_to_save)
-                    save_npy(data, h5_fname, fields, type_to_save)
-                    for negate in [True, False]:
-                        save_dipha(data, h5_fname, negate, type_to_save)
+    np_fname = sys.argv[1]
+    dipha_fname = sys.argv[2]
+    negate = False
+
+    convert_np_to_dipha(np_fname, dipha_fname, type_to_save=np.float64, negate=negate)
+
